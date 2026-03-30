0c8a6dd7fd86f4e3ab4d73c65566b6d49ee37d02
sai-rohith-bv <rohithgsal1@gmail.com>
2026-03-30 15:47:47 +0530

    feat(backend):Created and added Listing model, Added GET  /testListing test  route to verify DB save functionality Validated MongoDB connection via sample listing save

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const methodoverride = require('method-override');
 const mongoose = require('mongoose');
 const cors = require('cors');  // ← ADD THIS
+const Listing=require("./models/listing");
 
 const mongo_url = process.env.MONGO_URL;
 
@@ -24,6 +25,19 @@ app.use(cors());
 app.get("/", (req, res) => {
     res.send("hi,Welcome to our website");
 })
+
+app.get("/testListing",async(req,res)=>{
+     let samplelisting=new Listing({
+         title:"My new Villa",
+         description:"By the beach",
+         price:1220,
+         location:"Calangute , Goa",
+         country:"India",
+     });
+    await  samplelisting.save();
+    console.log("Sample was Saved");
+    res.send("Successfull testing");
+});
 
 app.listen(3030, () => {
     console.log("server is listening on port 3030....");
