ec64f5c84b58165619a4bd209737e04d91603aa1
sai-rohith-bv <rohithgsal1@gmail.com>
2026-05-10 13:05:05 +0530

    Created POST METHOD for sending Reviews data to server and to DataBase

--- Backend/schema.js
+++ Backend/schema.js
@@ -8,10 +8,9 @@ module.exports.listingSchema = Joi.object({
     price: Joi.number().required().min(0),
     image: Joi.object({
         url: Joi.string().allow("", null),
-        filename: Joi.string().allow("", null)
+          filename: Joi.string().allow("", null)
     }).required()
 });
-
 module.exports.reviewSchema = Joi.object({
     rating: Joi.number().integer().min(1).max(5).required(),
     comment: Joi.string().trim().min(12).max(500).required(),
