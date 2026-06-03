22259ceb7b57f5f0feadaa0a1df48f66753ae65f
sai-rohith-bv <rohithgsal1@gmail.com>
2026-06-03 14:30:55 +0530

    fix: configure cors to allow vercel frontend production origin

--- Backend/app.js
+++ Backend/app.js
@@ -44,7 +44,7 @@ app.use(cors({
         "http://127.0.0.1:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5174",
-        "https://your-future-vercel-app-name.vercel.app"
+        "https://stay-nest-phi.vercel.app"
     ],
     credentials: true
 }));
